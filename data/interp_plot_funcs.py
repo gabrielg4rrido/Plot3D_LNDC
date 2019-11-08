2f84e6cfa4adebba5a443d32d9aca84fe8effc19
Gabriel de Ávila Garrido <gabriel.garrido@lndc.metalmat.ufrj.br>
2019-11-08 14:38:00 -0300

    Criando subplots - superfícies e contornos

---
+++ data/interp_plot_funcs.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
+import plotly.subplots
+import scipy.interpolate as interp
+import numpy as np
+import mpl_toolkits.mplot3d
+import time
+import sys, os
+import plotly.offline
+from pylab import rcParams
+import matplotlib
+
+
+"""
+    Recebe como parâmetros x e y, arrays que definem os pontos coordenados usados para aproximar 
+    uma função f = z(x,y) e, por fim, plota na tela o gráfico em 3D de sua superfície, seu mapa de calor e seu mapa de contorno.
+"""
+
+def interp_plot(x, y, z, graph_title='Graph', interp_type='linear'):
+    rcParams['figure.figsize'] = (8, 6)  # Define o tamanho da figura do gráfico
+    begin = time.time()
+    matplotlib.use('qt5agg')
+
+    # Iguala o shape dos arrays
+    # XX = matriz onde todas as linhas são iguais ao array X
+    # YY = matriz onde todas as colunas são iguais ao array Y
+    XX, YY = np.meshgrid(x, y)  
+
+    f = interp.interp2d(x, y, z, kind=interp_type)
+    n_points = 500
+
+    # Cria um array ordenado igualmente espaçado de tamanho = n_points no intervalo [menor valor(x), maior valor(x)]
+    xnew = np.linspace(start=min(x), stop=max(x), num=n_points)
+
+    # Cria um array ordenado igualmente espaçado de tamanho = n_points no intervalo [menor valor(y), maior valor(y)]
+    ynew = np.linspace(start=min(y), stop=max(y), num=n_points)  
+    
+    # Obtém os pontos de z através da função interpolada e dos pontos coordenados criados por xnew e ynew
+    znew = f(xnew, ynew)
+
+    XXnew, YYnew = np.meshgrid(xnew, ynew)
+
+    plt.figure()
+    ax1 = plt.axes([0.05, 0.05, 0.9, 0.9], projection='3d')  # Cria e define os limites da superfície superfície
+    ax1.plot_surface(XX, YY, z)  # Plota a superfície
+    ax1.set(title=graph_title, xlabel='X Axis', ylabel='Y Axis', zlabel='Z Axis')
+
+    plt.figure()
+    ax2 = plt.axes([0.05, 0.05, 0.9, 0.9], projection='3d')  # Define a superfície
+    surface = ax2.plot_surface(XXnew, YYnew, znew, rstride=1,
+                               cstride=1, cmap='jet', linewidth=0.25)  # Plota a superfície adicionando a ela seu mapa de calor
+    ax2.set(title=graph_title, xlabel='X Axis', ylabel='Y Axis', zlabel='Z Axis')
+    plt.colorbar(surface, shrink=0.5, aspect=5)
+
+    plt.figure()
+    plt.contourf(x, y, z, cmap='jet')  # Plota o mapa de contorno da função
+    plt.colorbar()  # Adiciona uma barra de calor ao gráfico
+    plt.title("{}  - Contours".format(graph_title))
+    plt.show()
+
+    end = time.time()
+    print("Tempo de execução: {}".format(end - begin))
+
+
+
+def plot_window_plotly(x, y, z, c1='blues', c2='jet', smooth=True, title='Graph'):
+    from PyQt5.QtCore import QUrl
+    from PyQt5.QtWebEngineWidgets import QWebEngineView
+    from PyQt5.QtWidgets import QApplication
+
+    fig = plotly.subplots.make_subplots(rows=2, cols=2,
+                                        specs=[[{'type' : 'surface'}, {'type' : 'surface'}],
+                                                [{'type' : 'contour'}, {'type' : 'heatmap' }]])
+
+    fig.add_trace(go.Surface(x=x, y=y, z=z, colorscale=c1, showscale=False), row=1, col=1)
+    fig.update_traces(contours_z=dict(show=True, usecolormap=True, highlightcolor="limegreen", project_z=True))
+    fig.add_trace(go.Surface(x=x, y=y, z=z, colorscale=c2), row=1, col=2)
+    fig.update_traces(contours_z=dict(show=True, usecolormap=True, highlightcolor="limegreen", project_z=True))
+
+    if(smooth):
+        fig.add_trace(go.Heatmap(x=x, y=y, z=z, colorscale=c1, showscale=False, zsmooth='best', connectgaps=True), row=2, col=1)
+        fig.add_trace(go.Heatmap(x=x, y=y, z=z, colorscale=c2, showscale=False, zsmooth='best', connectgaps=True), row=2, col=2)
+    else:
+        fig.add_trace(go.Contour(x=x, y=y, z=z, colorscale=c1, showscale=False, line_width=0), row=2, col=1)
+        fig.add_trace(go.Contour(x=x, y=y, z=z, colorscale=c2, showscale=False, line_width=0), row=2, col=2)
+
+    fig.update_layout(title=title,  margin=dict(l=65, r=50, b=65, t=90), height=800, width=1000)
+
+
+    plotly.offline.plot(fig, filename='graph_plot.html', auto_open=False)
+
+    app = QApplication(sys.argv)
+    web = QWebEngineView()
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "graph_plot.html"))
+    web.load(QUrl.fromLocalFile(file_path))
+    web.resize(1050, 850)
+    web.show()
+    sys.exit(app.exec_())
--- data/plot_3d.py
+++ data/plot_3d.py
@@ -1,27 +1,5 @@
 import numpy as np
-import plotly.graph_objs as go
-import sys, os
-import plotly.offline
-import  interp3dfunc
-import matplotlib
-
-
-def interp_plotly(fig):
-    from PyQt5.QtCore import QUrl
-    from PyQt5.QtWebEngineWidgets import QWebEngineView
-    from PyQt5.QtWidgets import QApplication
-
-    plotly.offline.plot(fig, filename='graph_plot.html', auto_open=False)
-
-    app = QApplication(sys.argv)
-    web = QWebEngineView()
-    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "graph_plot.html"))
-    web.load(QUrl.fromLocalFile(file_path))
-    web.show()
-    web.setWindowTitle("Graph")
-    sys.exit(app.exec_())
-    sys.quit()
-
+import  interp_plot_funcs
 
 nomeArq = 'EstudoAntigo2_difAmpZ.txt'
 cabecalho = 15
@@ -53,12 +31,5 @@ y = list(range(rotVal[0], rotVal[1]+1, rotVal[2]))
 z = dataArray
 
 
-surface = go.Figure(data=[go.Surface(x=x, y=y ,z=z , colorscale="Jet")])
-surface.update_traces(contours_z=dict(show=True, usecolormap=True, highlightcolor="limegreen", project_z=True))
-surface.update_layout(title='Graph', margin=dict(l=65, r=50, b=90, t=90))
-                  #scene_camera_eye=dict(x=1.87, y=0.88, z=-0.64),
-                  #margin=dict(l=65, r=50, b=65, t=90))
-
-#contour = go.Figure(data = [go.Contour(x=x, y=y, z=z, colorscale="Jet")])
-
-interp_plotly(surface)
+interp_plot_funcs.plot_window_plotly(x, y, z, title='Plot 3D', smooth=True)
+#interp_plot_funcs.interp_plot(x, y, z)
